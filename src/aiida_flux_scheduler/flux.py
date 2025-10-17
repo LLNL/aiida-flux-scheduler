d3181ebb1260b834f79eb3251a7d5fd60949df64
Nathan Keilbart <keilbart1@llnl.gov>
2025-10-17 10:09:12 -0700

    Updated validate_resources.

--- src/aiida_flux_scheduler/flux.py
+++ src/aiida_flux_scheduler/flux.py
@@ -43,7 +43,21 @@ class FluxJobResource(NodeNumberJobResource):
         :return: attribute dictionary with the parsed parameters populated.
         """
 
+        mws = kwargs.pop('max_wallclock_seconds', None)
+        try:
+            mws = int(mws)
+        except ValueError:
+            raise ValueError(
+                '`max_wallclock_seconds` should be set as an int.'
+            )
+        except TypeError:
+            raise TypeError(
+                '`max_wallclock_seconds` was set as None. Provide a value in '
+                'metadata.options.persistent_resources.'
+            )
+
         resources = super().validate_resources(**kwargs)
+        resources['max_wallclock_seconds'] = mws
 
         return resources
     
